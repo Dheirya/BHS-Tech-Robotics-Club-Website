8bc0acebd288a0c68ea481f0e27aab0dd83c7b0c
Dheirya Tyagi <69175561+Dheirya@users.noreply.github.com>
2025-05-26 19:17:50 -0400

    Update constants.h

--- hardware/constants.h
+++ hardware/constants.h
@@ -17,16 +17,31 @@
 #define TS_MINY 70
 #define TS_MAXY 920
 #define BLACK 0x0000
+#define LIGHTBLACK 0x4a69
 #define WHITE 0xFFFF
 #define SKYBLUE 0x265f
 #define DARKTHEME 0x65ba
 #define THEME 0x0436
-#define GREEN 0x1e4c
+#define GREEN 0x2589
+#define LIGHTGREEN 0x6650
 #define BACKGROUND 0xdfbf
 #define YELLOW 0xfe8b
+#define BROWN 0x9a86
+#define DARKBROWN 0x4965
 #define DARKYELLOW 0xfe47
 #define MINPRESSURE 10
 #define MAXPRESSURE 1000
+#define MAX_STARS 150
+
+int numStars = 0;
+int starX[MAX_STARS];
+int starY[MAX_STARS];
+bool starVisible[MAX_STARS];
+unsigned long starTimers[MAX_STARS];
+unsigned long starIntervals[MAX_STARS];
+unsigned long lastStarUpdate = 0;
+const int minStarInterval = 1000;
+const int maxStarInterval = 20000;
 
 MCUFRIEND_kbv tft;
 TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
@@ -41,7 +56,15 @@ unsigned long previousMillis = 0;
 unsigned long blinkStartTime = 0;
 unsigned long nextBlinkTime = 0;
 const long blinkDuration = 200;
-const long minBlinkInterval = 5000;
-const long maxBlinkInterval = 10000;
+const long minBlinkInterval = 1000;
+const long maxBlinkInterval = 5000;
 unsigned long lastButtonPressTime = 0;
 const long buttonCooldown = 500;
+uint32_t seed = 12345;
+
+uint16_t fastRandom(uint16_t minVal, uint16_t maxVal) {
+  seed ^= seed << 13;
+  seed ^= seed >> 17;
+  seed ^= seed << 5;
+  return minVal + (seed % (maxVal - minVal));
+}
